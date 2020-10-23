f7bfbcaecbcd73363c1b6dbc0cbe41da8a8548bb
Jean-Bernard PERIDY <jbperidy@hotmail.fr>
2020-10-23 16:43:38 +0200

    remove from cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { Col, Row, ListGroup, Image, Form, Button, Card} from 'react-bootstrap';
 import Message from '../components/Message';
-import { addToCart } from '../actions/cartActions';
+import { addToCart, removeFromCart } from '../actions/cartActions';
 
 const CartScreen = ({ match, location, history }) => {
     const productID = match.params.id;
@@ -22,7 +22,7 @@ const CartScreen = ({ match, location, history }) => {
     }, [dispatch, productID, qty]);
 
     const removeFromCartHandler = (id) => {
-        console.log('remove');
+        dispatch(removeFromCart(id));
     };
 
     const checkOutHandler = () => {
@@ -38,7 +38,7 @@ const CartScreen = ({ match, location, history }) => {
                         Your cart is empty <Link to='/'>Go Back</Link>
                     </Message>) : (
                         <ListGroup variant='flush'>
-                            {cartItems.map( item => (
+                            {cartItems.map(item => (
                                 <ListGroup.Item key={item.product}>
                                     <Row>
                                         <Col md={2}>
