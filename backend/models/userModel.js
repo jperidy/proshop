9715758bec1e548ded1f05e775c14a72c086942e
Jean-Bernard PERIDY <jbperidy@hotmail.fr>
2020-10-27 18:58:26 +0100

    User login reducer and actions

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,29 +4,42 @@ import bcrypt from 'bcryptjs'
 const userSchema = mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
     },
     password: {
         type: String,
-        required: true
+        required: true,
     },
     isAdmin: {
         type: Boolean,
         required: true,
-        default: false
+        default: false,
     }
 }, {
-    timestamps: true
+    timestamps: true,
 });
 
+// method to verify password
 userSchema.methods.matchPassword = async function(enteredPassword){
     return await bcrypt.compare(enteredPassword, this.password);
 };
+
+// method to execut before any save in database if password is modified
+userSchema.pre('save', async function (next) {
+
+    if(!this.isModified('password')){
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
 
 const User = mongoose.model('User', userSchema);
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,18 +3,23 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
+import { userLoginReducer } from './reducers/userReducers';
 
 // la fonction combineReducers, permet d'englober tous les reducers dans un reducer root.
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
 
+const userItemsFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null;
+
 const initialState = {
-    cart: { cartItems: cartItemsFromStorage }
+    cart: { cartItems: cartItemsFromStorage },
+    userLogin: {userInfo: userItemsFromStorage}
 };
 
 const middleware = [thunk];
