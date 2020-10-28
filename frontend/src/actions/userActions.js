d36cbdb964591a1375ea708799053593c09224c3
Jean-Bernard PERIDY <jbperidy@hotmail.fr>
2020-10-28 15:46:46 +0100

    LoginScreen creation

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../constants/userConstants";
 
-export const login = (email, password) => async (dispatch) {
+export const login = (email, password) => async(dispatch) => {
     try {
         dispatch({
             type: USER_LOGIN_REQUEST,
-
         });
 
         const config = {
