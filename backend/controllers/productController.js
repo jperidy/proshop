8a2c8655199a267274e2f6bddb3967a9fcfdf320
Jean-Bernard PERIDY <jbperidy@hotmail.fr>
2020-11-08 19:38:00 +0100

    product pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,6 +6,9 @@ import Product from '../models/productModel.js';
 // @access  Public
 const getProducts = asyncHandler(async(req,res) =>{
 
+    const pageSize = 10;
+    const page = Number(req.query.pageNumber) || 1; // by default on page 1
+
     const keyword = req.query.keyword ? {
         name: {
             $regex: req.query.keyword,
@@ -13,10 +16,11 @@ const getProducts = asyncHandler(async(req,res) =>{
         }
     } : {};
 
-    const products = await Product.find({ ...keyword });
+    const count = await Product.countDocuments({ ...keyword });
+    const products = await Product.find({ ...keyword }).limit(pageSize).skip(pageSize * (page - 1));
     // uncomment the line for simulate errors
     //throw new Error('Some error');
-    res.json(products);
+    res.json({ products, page, pages: Math.ceil(count / pageSize) });
 })
 
 // @desc    Fetch sigle product
