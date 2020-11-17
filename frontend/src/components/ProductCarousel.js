3e007c6e076071de549dd1f6ac59ab39342257fd
Jean-Bernard PERIDY <jbperidy@hotmail.fr>
2020-11-17 19:56:21 +0100

    Caroussel modification for mobile

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -22,9 +22,9 @@ const ProductCarousel = () => {
             {products.map(product => (
                 <Carousel.Item key={product._id}>
                     <Link to={`/product/${product._id}`}>
-                        <Image src={product.image} alt={product.name} fluid />
+                        <Image className='m-0 p-10' src={product.image} alt={product.name} fluid/>
                         <Carousel.Caption className='carousel-caption'>
-                            <h2>{product.name} (${product.price})</h2>
+                            <h2 style={{color:'grey'}}>{product.name} (${product.price})</h2>
                         </Carousel.Caption>
                     </Link>
                 </Carousel.Item>
